e3fc7375b524d8d670482fe2d81f69d085100ca6
Hieu Vu <vdhieu.it@gmail.com>
2021-02-27 17:38:05 +0700

    chore: project config

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function IndexPage() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-5xl text-purple-500 text-center">Hello world</h1>
+    </div>
+  );
+}
